644ab900676a686362cbf2c1e33b4a640f287f9b
Roman Messer <rms@gotocloud.net>
2015-01-29 21:58:22 +0100

    Refactor code

--- example-01.rb
+++ example-01.rb
@@ -1,20 +1,16 @@
 require 'open-uri'
 require 'json'
 
-puts 'Hello World!'
-
 print 'Where would you like to travel today?'
 location = gets.chomp.capitalize
 
-print "Location is #{ location }.\n"
-
-
-#file = open("http://lemonde.fr")
 file = open("http://api.openweathermap.org/data/2.5/weather?q=" + location)
 contents = file.read
-#puts contents.inspect
+
 
 parsed = JSON.parse(contents)
 
 weather = parsed["weather"]
-puts weather[0].fetch('description')
+weather_description = weather[0].fetch('description')
+
+puts "The weather in #{ location } is : #{weather_description}"
